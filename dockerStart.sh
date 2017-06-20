f247db548ae2f117f37fb0dc061572a50d0f2282
Meenakshi-2017 <meenakshi.rani@ril.com>
2017-06-20 11:59:55 +0530

    Create dockerStart.sh

---
+++ dockerStart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+#apt-get install -y --allow-unauthenticated docker-engine 
+
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu trusty stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+#DNS IP address configuration
+echo "nameserver "8.8.8.8 >> /etc/resolvconf/resolv.conf.d/head
+resolvconf -u
+echo "Downloading Docker Image and Run"
+sudo docker login -u meenakshi2017 -p ril123
+docker pull meenakshi2017/server   
