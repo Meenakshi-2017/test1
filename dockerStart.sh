3ab07f8078a706d79b9f58997a8db18b417bbf95
Meenakshi-2017 <meenakshi.rani@ril.com>
2017-07-06 16:33:41 +0530

    for ps

--- dockerStart.sh
+++ dockerStart.sh
@@ -13,5 +13,8 @@ echo "nameserver "8.8.8.8 >> /etc/resolvconf/resolv.conf.d/head
 resolvconf -u
 echo "Downloading Docker Image and Run"
 sudo docker login -u meenakshi2017 -p ril123
-docker pull meenakshi2017/server 
-docker run  -p 8086:8086 -it meenakshi2017/server
+docker pull meenakshi2017/vprovserv:1.0
+docker network create --driver=bridge --subnet=172.16.129.0/24 --gateway=172.16.129.1 psnet
+docker run --ip=172.16.129.67 --net=psnet --name=vPs -p 1200:1200 meenakshi2017/vprovserv:1.0
+#docker pull meenakshi2017/server 
+#docker run  -p 8086:8086 -it meenakshi2017/server
